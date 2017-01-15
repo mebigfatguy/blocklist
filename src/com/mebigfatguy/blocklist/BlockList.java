0c6db10f29e53a16fa32e18087b8655caaa17194
Dave Brosius <dbrosius@mebigfatguy.com>
2017-01-15 16:05:57 -0500

    update cp dates

--- src/com/mebigfatguy/blocklist/BlockList.java
+++ src/com/mebigfatguy/blocklist/BlockList.java
@@ -1,6 +1,6 @@
 /*
  * BlockList an alternative java.util.List
- * Copyright 2011-2016 MeBigFatGuy.com
+ * Copyright 2011-2017 MeBigFatGuy.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
